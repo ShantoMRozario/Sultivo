482e6f2e631965a537a8465729186f83675971cd
ShantoMRozario <shantoxrozario@gmail.com>
2022-09-04 14:55:14 +0600

    responsive v1

--- js/script.js
+++ js/script.js
@@ -10,7 +10,32 @@ $(function(){
         maxWidth:"80%",
     });
 
+    // Counter UP
+    $('.shantocount').counterUp({
+        delay: 15,
+        time: 1500
+    });
 
-
-
+    // Slick Slider
+    $('.portfolio_left_slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.image_port'
+      });
+      $('.image_port').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.portfolio_left_slide',
+        focusOnSelect: true,
+        prevArrow: '.prev_arrow',
+        nextArrow: '.next_arrow',
+        autoplay: true ,
+        
+      });
+    
+    
+    
+    
 })
